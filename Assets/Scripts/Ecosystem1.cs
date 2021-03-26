e656dc56384c7bbfa73f8cebb09d641c9b46347a
fatjosephina <55261891+fatjosephina@users.noreply.github.com>
2021-03-25 19:33:45 -0500

    Started ecosystem

---
+++ Assets/Scripts/Ecosystem1.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ecosystem1 : MonoBehaviour
+{
+    // Declare a mover object
+    private EcoMover mover;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Create a Mover object
+        mover = new EcoMover();
+    }
+
+    // Update is called once per frame forever and ever (until you quit).
+    void Update()
+    {
+        mover.Update();
+        mover.CheckEdges();
+    }
+}
+
+public class EcoMover
+{
+    // The basic properties of a mover class
+    private Vector2 location, velocity, acceleration;
+    private float topSpeed;
+
+    // The window limits
+    private Vector2 minimumPos, maximumPos;
+
+
+    // Gives the class a GameObject to draw on the screen
+    private GameObject mover = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+    public EcoMover()
+    {
+        findWindowLimits();
+        location = Vector2.zero; // Vector2.zero is a (0, 0) vector
+        velocity = Vector2.zero;
+        acceleration = new Vector2(-0.1F, -0.1F);
+        topSpeed = 10F;
+
+        //We need to create a new material for WebGL
+        Renderer r = mover.GetComponent<Renderer>();
+        r.material = new Material(Shader.Find("Diffuse"));
+    }
+
+    float slither = 2f;
+    float timer = 2f;
+
+    public void Update()
+    {
+        timer -= Time.deltaTime;
+        Debug.Log(timer);
+        if (0f < timer && timer <= 1f)
+            slither *= -1f;
+        else if (timer <= 0f)
+            timer = 2f; slither *= -1f;
+        acceleration.x = 0f;
+        acceleration.x += slither;
+        velocity.x = 0f;
+        // Speeds up the mover
+        velocity += acceleration * Time.deltaTime; // Time.deltaTime is the time passed since the last frame.
+
+        velocity.x += slither;
+        // Limit Velocity to the top speed
+        velocity = Vector2.ClampMagnitude(velocity, topSpeed);
+
+        // Moves the mover
+        location += velocity * Time.deltaTime;
+
+        // Updates the GameObject of this movement
+        mover.transform.position = new Vector2(location.x, location.y);
+    }
+
+    public void CheckEdges()
+    {
+        if (location.x > maximumPos.x)
+        {
+            location.x -= maximumPos.x - minimumPos.x;
+        }
+        else if (location.x < minimumPos.x)
+        {
+            location.x += maximumPos.x - minimumPos.x;
+        }
+        if (location.y > maximumPos.y)
+        {
+            location.y -= maximumPos.y - minimumPos.y;
+        }
+        else if (location.y < minimumPos.y)
+        {
+            location.y += maximumPos.y - minimumPos.y;
+        }
+    }
+
+    private void findWindowLimits()
+    {
+        // The code to find the information on the camera as seen in Figure 1.2
+
+        // We want to start by setting the camera's projection to Orthographic mode
+        Camera.main.orthographic = true;
+        // Next we grab the minimum and maximum position for the screen
+        minimumPos = Camera.main.ScreenToWorldPoint(Vector2.zero);
+        maximumPos = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+    }
+}
