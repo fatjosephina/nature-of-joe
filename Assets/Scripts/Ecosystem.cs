35be35081348321b26f61d5cb36296d930a0286c
fatjosephina <55261891+fatjosephina@users.noreply.github.com>
2021-04-12 17:11:26 -0500

    In class exercises

--- Assets/Scripts/Ecosystem.cs
+++ Assets/Scripts/Ecosystem.cs
@@ -8,29 +8,40 @@ public class Ecosystem : MonoBehaviour
     public List<GameObject> chapter1Creatures = new List<GameObject>();
     public GameObject chapter1Creature;
     public int chapter1CreaturePopulation;
+    public int chapter1MinimumPopulation;
 
     public List<GameObject> chapter2Creatures = new List<GameObject>();
     public GameObject chapter2Creature;
     public int chapter2CreaturePopulation;
+    public int chapter2MinimumPopulation;
 
     public List<GameObject> chapter3Creatures = new List<GameObject>();
     public GameObject chapter3Creature;
     public int chapter3CreaturePopulation;
+    public int chapter3MinimumPopulation;
 
     public List<GameObject> chapter6Creatures = new List<GameObject>();
     public GameObject chapter6Creature;
     public int chapter6CreaturePopulation;
+    public int chapter6MinimumPopulation;
 
     public List<GameObject> chapter7Creatures = new List<GameObject>();
     public GameObject chapter7Creature;
     public int chapter7CreaturePopulation;
+    public int chapter7MinimumPopulation;
 
     public List<GameObject> chapter8Creatures = new List<GameObject>();
     public GameObject chapter8Creature;
     public int chapter8CreaturePopulation;
+    public int chapter8MinimumPopulation;
 
     public PerlinTerrain terrain;
     public float terrainMin;
+
+    //Animal food
+    public GameObject flower;
+    public int flowerTotal;
+    public int flowerMin;
 
     // Start is called before the first frame update
     void Start()
@@ -45,6 +56,29 @@ public class Ecosystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (chapter1Creatures.Count <= chapter1MinimumPopulation)
+        {
+            StartCoroutine(circleOfLife(chapter1Creature, new Vector3(Random.Range(terrainMin, terrain.cols), Random.Range(4f, 20f), Random.Range(terrainMin, terrain.rows))));
+        }
+        if (chapter2Creatures.Count <= chapter2MinimumPopulation)
+        {
+            StartCoroutine(circleOfLife(chapter2Creature, new Vector3(Random.Range(terrainMin, terrain.cols), Random.Range(4f, 20f), Random.Range(terrainMin, terrain.rows))));
+        }
+    }
+
+    IEnumerator circleOfLife(GameObject prey, Vector3 position)
+    {
+        yield return new WaitForSeconds(10);
+        if (prey.name == chapter1Creature.name || prey.name == chapter1Creature.name + "(Clone)")
+        {
+            GameObject c = Instantiate(prey, position, Quaternion.identity);
+            chapter1Creatures.Add(c);
+        }
+        yield return new WaitForSeconds(10);
+        if (prey.name == chapter2Creature.name || prey.name == chapter2Creature.name + "(Clone)")
+        {
+            GameObject c = Instantiate(prey, position, Quaternion.identity);
+            chapter2Creatures.Add(c);
+        }
     }
 }
---
+++ Assets/Scripts/PredatorAttract.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PredatorAttract : MonoBehaviour
+{
+    public float gravityForce = 6.7f;
+    public float mass;
+    Vector3 location;
+    public GameObject predator;
+    public string predatorTag = "";
+    public bool isAlive = true;
+
+    Ecosystem ecosystem;
+    List<GameObject> chapterCreatures;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ecosystem = GameObject.Find("Scripts").GetComponent<Ecosystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isAlive)
+        {
+            GameObject[] predators = GameObject.FindGameObjectsWithTag(predatorTag);
+            if (predators.Length > 0)
+            {
+                foreach (GameObject predator in predators)
+                {
+                    location = this.gameObject.transform.position;
+                    predator.transform.GetComponent<Rigidbody>().AddForce(predator.transform.forward, ForceMode.Acceleration);
+                    predator.transform.GetComponent<Rigidbody>().AddForce(Attract(predator), ForceMode.Acceleration);
+
+                    float dist = Vector3.Distance(predator.transform.position, location);
+                    if (dist <= 4f)
+                    {
+                        isAlive = false;
+                        if (predatorTag == "Chapter1Predator")
+                        {
+                            ecosystem.chapter1Creatures.Remove(this.gameObject);
+                        }
+                        else if (predatorTag == "Chapter2Predator")
+                        {
+                            ecosystem.chapter2Creatures.Remove(this.gameObject);
+                        }
+                        else if (predatorTag == "Chapter3Predator")
+                        {
+                            ecosystem.chapter3Creatures.Remove(this.gameObject);
+                        }
+                        else if (predatorTag == "Chapter6Predator")
+                        {
+                            ecosystem.chapter6Creatures.Remove(this.gameObject);
+                        }
+                        else if (predatorTag == "Chapter7Predator")
+                        {
+                            ecosystem.chapter7Creatures.Remove(this.gameObject);
+                        }
+                        else if (predatorTag == "Chapter8Predator")
+                        {
+                            ecosystem.chapter8Creatures.Remove(this.gameObject);
+                        }
+                        Destroy(gameObject);
+                    }
+                }
+            }
+        }
+    }
+
+    public Vector3 Attract(GameObject predator)
+    {
+        Vector3 difference = location - predator.transform.position;
+        float dist = difference.magnitude;
+        Vector3 gravityDirection = difference.normalized;
+        float gravity = gravityForce * (mass * predator.GetComponent<Rigidbody>().mass) / (dist * dist);
+        Vector3 gravityVector = gravityDirection * gravity;
+
+        return gravityVector;
+    }
+}
